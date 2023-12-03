e4d578e2ed535ee87716aa862d729af6a031c7ca
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-12-03 13:07:20 +0100

    task02 src, tests & input added

---
+++ Advent23/src/Task02.cs
@@ -0,0 +1,142 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent23;
+
+public class Roll
+{
+    public Roll(string rollData)
+    {
+        var matches = Regex.Matches(rollData, @"(-?\d+) (\w+)");
+
+        foreach (Match match in matches)
+        {
+            int count = int.Parse(match.Groups[1].Value);
+
+            if (count < 0)
+                throw new ArgumentException("Count cannot be negative");
+
+            switch (match.Groups[2].Value.ToLower())
+            {
+                case "blue":
+                    if (Blue != 0)
+                        throw new ArgumentException("Cannot have two blue values in one roll");
+                    Blue += count;
+                    break;
+                case "red":
+                    if (Red != 0)
+                        throw new ArgumentException("Cannot have two red values in one roll");
+                    Red += count;
+                    break;
+                case "green":
+                    if (Green != 0)
+                        throw new ArgumentException("Cannot have two green values in one roll");
+                    Green += count;
+                    break;
+            }
+        }
+
+        if (Blue == 0 && Red == 0 && Green == 0)
+            throw new ArgumentException("Roll cannot be empty");
+    }
+
+    public Roll(int blue, int red, int green)
+    {
+        Blue = blue;
+        Red = red;
+        Green = green;
+    }
+
+    public int Blue { get; set; }
+    public int Red { get; set; }
+    public int Green { get; set; }
+}
+
+public class SingleGame
+{
+    public int GameIndex { get; set; }
+    public List<Roll> Rolls { get; set; } = new List<Roll>();
+
+    public Roll GetMaxValues()
+    {
+        return new Roll(
+            Rolls.Max(r => r.Blue),
+            Rolls.Max(r => r.Red),
+            Rolls.Max(r => r.Green)
+        );
+    }
+
+    public bool IsValid(Roll maxValues)
+    {
+        return GetMaxValues().Blue <= maxValues.Blue
+               && GetMaxValues().Red <= maxValues.Red
+               && GetMaxValues().Green <= maxValues.Green;
+    }
+}
+
+public class Games
+{
+    private Games(List<SingleGame> singleGames, Roll maxValues)
+    {
+        SingleGames = singleGames;
+        MaxValues = maxValues;
+    }
+
+    public List<SingleGame>? SingleGames { get; private set; }
+    public Roll MaxValues { get; private set; }
+
+    public static Games ParseFromFile(string filePath, Roll maxValues)
+    {
+        var singleGames = new List<SingleGame>();
+
+        string[] lines = File.ReadAllLines(filePath);
+
+        foreach (var line in lines)
+        {
+            var singleGame = new SingleGame();
+            var gameData = line.Split(':');
+            singleGame.GameIndex = int.Parse(Regex.Match(gameData[0], @"\d+").Value);
+
+            var rollsData = gameData[1].Split(';');
+
+            foreach (var rollData in rollsData)
+                singleGame.Rolls.Add(new Roll(rollData));
+
+            singleGames.Add(singleGame);
+        }
+
+        return new Games(singleGames, maxValues);
+    }
+
+    public static Games ParseFromString(string gamesData, Roll maxValues)
+    {
+        var singleGames = new List<SingleGame>();
+
+        var games = gamesData.Split("\r\n");
+
+        foreach (var game in games)
+        {
+            var singleGame = new SingleGame();
+            var gameData = game.Split(':');
+            singleGame.GameIndex = int.Parse(Regex.Match(gameData[0], @"\d+").Value);
+
+            var rollsData = gameData[1].Split(';');
+
+            foreach (var rollData in rollsData)
+                singleGame.Rolls.Add(new Roll(rollData));
+
+            singleGames.Add(singleGame);
+        }
+
+        return new Games(singleGames, maxValues);
+    }
+
+    public static Games Create(List<SingleGame> singleGames, Roll maxValues)
+    {
+        return new Games(singleGames, maxValues);
+    }
+
+    public int GetIdxSumOfValidGames()
+    {
+        return SingleGames.Where(g => g.IsValid(MaxValues)).Sum(g => g.GameIndex);
+    }
+}
---
+++ Advent23/test/Task02Test.cs
@@ -0,0 +1,179 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Advent23;
+
+public class Task02Test
+{
+    [Fact]
+    public void Test_parsing_a_roll()
+    {
+        var roll = new Roll("1 blue 2 red 3 green");
+
+        roll.Blue.Should().Be(1);
+        roll.Red.Should().Be(2);
+        roll.Green.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test_parsing_a_roll_with_two_values_of_same_type()
+    {
+        Action act = () => new Roll("1 blue 2 blue 3 green");
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Test_creating_an_empty_roll()
+    {
+        Action act = () => new Roll(0, 0, 0);
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Test_parsing_a_roll_with_negative_values()
+    {
+        Action act = () => new Roll("1 blue -2 red 3 green");
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Test_get_maximum_of_each_color_in_game()
+    {
+        var game = new SingleGame
+        {
+            GameIndex = 1,
+            Rolls = new List<Roll>
+            {
+                new(1, 2, 3),
+                new(4, 5, 6),
+                new(7, 8, 9)
+            }
+        };
+
+        var result = game.GetMaxValues();
+        result.Should().BeEquivalentTo(new Roll(7, 8, 9));
+    }
+
+    [Fact]
+    public void Test_parsing_games_from_string()
+    {
+        var input = "Game 1: 1 blue, 1 red\r\nGame 2: 1 blue, 1 green; 1 green, 1 blue, 1 red";
+
+        var games = Games.ParseFromString(input, new Roll(1, 1, 1));
+
+        games.Should().BeEquivalentTo(Games.Create(new List<SingleGame>
+        {
+            new()
+            {
+                GameIndex = 1,
+                Rolls = new List<Roll>
+                {
+                    new(1, 1, 0)
+                }
+            },
+            new()
+            {
+                GameIndex = 2,
+                Rolls = new List<Roll>
+                {
+                    new(1, 0, 1),
+                    new(1, 1, 1)
+                }
+            }
+        }, new Roll(1, 1, 1)));
+    }
+
+    [Fact]
+    public void Test_parsing_games_from_file()
+    {
+        var games = Games.ParseFromFile("C:\\Users\\nvojisla\\source\\repos\\Advent23\\Advent23\\res\\task02_test-small.txt", new Roll(1, 1, 1));
+
+        games.Should().BeEquivalentTo(Games.Create(new List<SingleGame>
+        {
+            new()
+            {
+                GameIndex = 1,
+                Rolls = new List<Roll>
+                {
+                    new(1, 1, 0)
+                }
+            },
+            new()
+            {
+                GameIndex = 2,
+                Rolls = new List<Roll>
+                {
+                    new(1, 0, 1)
+                }
+            }
+        }, new Roll(1, 1, 1)));
+    }
+
+    [Fact]
+    public void Test_if_single_games_are_identified_as_valid_and_invalid()
+    {
+        var games = Games.Create(new List<SingleGame>
+        {
+            new()
+            {
+                GameIndex = 1,
+                Rolls = new List<Roll>
+                {
+                    new(1, 1, 0)
+                }
+            },
+            new()
+            {
+                GameIndex = 2,
+                Rolls = new List<Roll>
+                {
+                    new(1, 0, 1)
+                }
+            }
+        }, new Roll(1, 1, 0));
+
+        games.SingleGames.Should().HaveCount(2);
+        games.SingleGames?[0].IsValid(games.MaxValues).Should().BeTrue();
+        games.SingleGames?[1].IsValid(games.MaxValues).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test_if_sum_of_indices_of_valid_games_is_calculated_correctly()
+    {
+        var games = Games.Create(new List<SingleGame>
+        {
+            new()
+            {
+                GameIndex = 1,
+                Rolls = new List<Roll>
+                {
+                    new(1, 1, 0)
+                }
+            },
+            new()
+            {
+                GameIndex = 2,
+                Rolls = new List<Roll>
+                {
+                    new(1, 0, 1)
+                }
+            }
+        }, new Roll(1, 1, 0));
+
+        games.GetIdxSumOfValidGames().Should().Be(1);
+    }
+
+    [Fact]
+    public void Test_if_sum_of_indices_of_valid_games_is_calculated_correctly_for_larger_input()
+    {
+        var games = Games.ParseFromFile("C:\\Users\\nvojisla\\source\\repos\\Advent23\\Advent23\\res\\task02.txt", new Roll(12, 13, 14));
+        games.GetIdxSumOfValidGames().Should().Be(8);
+    }
+
+    [Fact]
+    public void Test_puzzle_input()
+    {
+        var games = Games.ParseFromFile("C:\\Users\\nvojisla\\source\\repos\\Advent23\\Advent23\\res\\task02_input.txt", new Roll(14, 12, 13));
+        games.GetIdxSumOfValidGames().Should().Be(2377);
+    }
+}
