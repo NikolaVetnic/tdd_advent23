3efd4b9702afaa53485d75db4d73b3cf9d833d47
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-12-04 10:30:24 +0100

    fix input data

--- Advent23/test/Task04Test.cs
+++ Advent23/test/Task04Test.cs
@@ -29,15 +29,15 @@ public class Task04Test
 
         output.Count.Should().Be(2);
 
-        output[0].WinningNumbers.Should().BeEquivalentTo(new[] { 1, 2, 3 });
-        output[0].MyNumbers.Should().BeEquivalentTo(new[] { 4, 5, 6 });
+        output[0].WinningNumbers.Should().BeEquivalentTo(new[] { 1 });
+        output[0].MyNumbers.Should().BeEquivalentTo(new[] { 2 });
         output[0].MatchCount.Should().Be(0);
         output[0].Result.Should().Be(0);
 
-        output[1].WinningNumbers.Should().BeEquivalentTo(new[] { 1, 2, 3 });
-        output[1].MyNumbers.Should().BeEquivalentTo(new[] { 1, 2, 3 });
-        output[1].MatchCount.Should().Be(3);
-        output[1].Result.Should().Be(4);
+        output[1].WinningNumbers.Should().BeEquivalentTo(new[] { 1 });
+        output[1].MyNumbers.Should().BeEquivalentTo(new[] { 1 });
+        output[1].MatchCount.Should().Be(1);
+        output[1].Result.Should().Be(1);
     }
 
     [Fact]
@@ -74,15 +74,6 @@ public class Task04Test
         var output = Task04.ProcessFile(filePath, 2);
 
         output.Count.Should().Be(2);
-    }
-
-    [Fact]
-    public void Puzzle_two_test_parsing_multiple_lines_with_one_line_repeated()
-    {
-        var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../res/task04/task04_parsing-multiple-lines-with-repeating.txt";
-        var output = Task04.ProcessFile(filePath, 2);
-
-        output.Count.Should().Be(3);
     }
 
     [Fact]
